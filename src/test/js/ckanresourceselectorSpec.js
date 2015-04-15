eca025bcffce9f7ccce0fd59c0368228c4000077
Iris Muñoz <imunoz@conwet.com>
2015-04-15 14:01:26 +0200

    Prepare structure for testing

---
+++ src/test/js/ckanresourceselectorSpec.js
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2014-2015 CoNWeT Lab., Universidad Politécnica de Madrid
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*global $*/
+
+
+(function () {
+
+    "use strict";
+
+    jasmine.getFixtures().fixturesPath = 'src/test/fixtures/';
+
+    var dependencyList = [
+        'script',
+        'div#jasmine-fixtures',
+        'div.jasmine_html-reporter'
+    ];
+
+    var clearDocument = function clearDocument() {
+        $('body > *:not(' + dependencyList.join(', ') + ')').remove();
+    };
+
+    var getWiringCallback = function getWiringCallback(endpoint) {
+        var calls = MashupPlatform.wiring.registerCallback.calls;
+        var count = calls.count();
+        for (var i = count - 1; i >= 0; i--) {
+            var args = calls.argsFor(i);
+            if (args[0] === endpoint) {
+                return args[1];
+            }
+        }
+        return null;
+    };
+
+    describe("CKAN Resource Selector widget", function () {
+
+        beforeEach(function () {
+            loadFixtures('index.html');
+        });
+
+        afterEach(function () {
+            clearDocument();
+        });
+
+    });
+
+})();
